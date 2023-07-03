Alef-Burzmali/netbox-data-flows

commit a9393caf932e52f3a9d250d42a4af523f898c23b

Revamp the TabView on built-in models
* Regroup the DataFlow and ObjectAlias tabs in a single tab
* Split in two tables for dataflows where the object is a source or a destination
* Add some __init_subclass__ magic to avoid code duplication with every model

Fixes #19

netbox_data_flows/views/model_tabs.py
@@ -1,0 +1,106 @@
+from django.db.models import Count
+
+from netbox.views import generic
+from utilities.views import ViewTab, register_model_view
+
+from dcim.models import Device
+from ipam.models import IPAddress, IPRange, Prefix
+from virtualization.models import VirtualMachine
+
+from netbox_data_flows import models, tables
+
+
+__all__ = tuple()
+
+
+# NetBox models where we want to add a tab view
+MODELS = (Device, VirtualMachine, IPAddress, IPRange, Prefix)
+
+
+def _count_related_aliases_or_dataflows(obj):
+    aliases = models.ObjectAlias.objects.contains(obj).count()
+    if not aliases:
+        return 0  # cannot have a dataflow without an alias
+
+    dataflows = models.DataFlow.objects.sources_or_destinations(obj).count()
+    # return as string so "0" is considered non-empty
+    return str(dataflows)
+
+
+class DataFlowListTabViewBase(generic.ObjectView):
+    """
+    Add a tab with ObjectAlias and DataFlows to built-in
+    models
+    """
+
+    def __init_subclass__(cls, /, model, **kwargs):
+        """Create a subclass associated to a NetBox model"""
+
+        super().__init_subclass__(**kwargs)
+
+        # map the queryset to our NetBox model
+        cls.queryset = model.objects.all()
+
+        # call the decorator to register the view
+        register_model_view(
+            model,
+            name="dataflows-tab",
+            path="dataflows",
+        )(cls)
+
+    queryset = None
+    template_name = "netbox_data_flows/dataflow_tab.html"
+    actions = ("changelog",)
+
+    tab = ViewTab(
+        label="Data Flows",
+        permission="netbox_data_flows.view_dataflow",
+        badge=_count_related_aliases_or_dataflows,
+        hide_if_empty=True,
+    )
+
+    def get_extra_context(self, request, parent):
+        aliases_table = tables.ObjectAliasTable(
+            models.ObjectAlias.objects.annotate(
+                target_count=Count("targets", distinct=True),
+            ).contains(parent)
+        )
+        aliases_table.configure(request)
+
+        dataflow_sources_table = tables.DataFlowTable(
+            models.DataFlow.objects.sources(parent).prefetch_related(
+                "application",
+                "application__role",
+                "group",
+                "sources",
+                "destinations",
+            )
+        )
+        dataflow_sources_table.configure(request)
+
+        dataflow_destinations_table = tables.DataFlowTable(
+            models.DataFlow.objects.destinations(parent).prefetch_related(
+                "application",
+                "application__role",
+                "group",
+                "sources",
+                "destinations",
+            )
+        )
+        dataflow_destinations_table.configure(request)
+
+        return {
+            "aliases_table": aliases_table,
+            "dataflow_sources_table": dataflow_sources_table,
+            "dataflow_destinations_table": dataflow_destinations_table,
+        }
+
+
+for model in MODELS:
+    # create a subclass of DataFlowListTabViewBase per model
+    type(
+        f"{model}DataFlowTabView",
+        (DataFlowListTabViewBase,),
+        {},
+        model=Device,
+    )

netbox_data_flows/models/dataflows.py
@@ -50,10 +50,20 @@ class DataFlowQuerySet(RestrictedQuerySet):
         )
         return self.filter(group_id__in=subgroups)
 
-    def source_or_destination(self, *targets):
+    def sources_or_destinations(self, *targets):
         return self.filter(
             models.Q(sources__in=ObjectAlias.objects.contains(*targets))
             | models.Q(destinations__in=ObjectAlias.objects.contains(*targets))
+        ).distinct()
+
+    def sources(self, *targets):
+        return self.filter(
+            models.Q(sources__in=ObjectAlias.objects.contains(*targets))
+        ).distinct()
+
+    def destinations(self, *targets):
+        return self.filter(
+            models.Q(destinations__in=ObjectAlias.objects.contains(*targets))
         ).distinct()
 
 

netbox_data_flows/templates/netbox_data_flows/dataflow_tab.html
@@ -4,15 +4,40 @@
 {% load perms %}
 
 {% block content %}
-    {% include 'inc/table_controls_htmx.html' with table_modal="DataFlowTable_config" %}
-    <div class="card">
-        <div class="card-body" id="object_list">
-            {% include 'htmx/table.html' %}
+  <div class="row mb-3">
+    <div class="col col-md-12">
+      <div class="card">
+        <h5 class="card-header">Object Aliases</h5>
+        <div class="card-body table-responsive">
+          {% render_table aliases_table %}
         </div>
+      </div>
     </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col col-md-12">
+      <div class="card">
+        <h5 class="card-header">Source in Data Flows</h5>
+        <div class="card-body table-responsive">
+          {% render_table dataflow_sources_table %}
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col col-md-12">
+      <div class="card">
+        <h5 class="card-header">Destination in Data Flows</h5>
+        <div class="card-body table-responsive">
+          {% render_table dataflow_destinations_table %}
+        </div>
+      </div>
+    </div>
+  </div>
 {% endblock %}
 
 {% block modals %}
     {{ block.super }}
-    {% table_config_form table %}
 {% endblock modals %}

netbox_data_flows/templates/netbox_data_flows/objectalias_tab.html
@@ -1,18 +1,0 @@
-{% extends 'generic/object.html' %}
-{% load helpers %}
-{% load render_table from django_tables2 %}
-{% load perms %}
-
-{% block content %}
-    {% include 'inc/table_controls_htmx.html' with table_modal="ObjectAliasTable_config" %}
-    <div class="card">
-        <div class="card-body" id="object_list">
-            {% include 'htmx/table.html' %}
-        </div>
-    </div>
-{% endblock %}
-
-{% block modals %}
-    {{ block.super }}
-    {% table_config_form table %}
-{% endblock modals %}

netbox_data_flows/views/__init__.py
@@ -3,3 +3,4 @@ from .applicationroles import *  # noqa: F401,F403
 from .dataflows import *  # noqa: F401,F403
 from .groups import *  # noqa: F401,F403
 from .objectaliases import *  # noqa: F401,F403
+from .model_tabs import *  # noqa: F401,F403

netbox_data_flows/views/dataflows.py
@@ -3,10 +3,6 @@ import itertools
 from netbox.views import generic
 from utilities.views import ViewTab, register_model_view
 
-from dcim.models import Device
-from ipam.models import IPAddress, IPRange, Prefix
-from virtualization.models import VirtualMachine
-
 from netbox_data_flows import filtersets, forms, models, tables
 
 
@@ -140,60 +136,3 @@ class DataFlowBulkDeleteView(generic.BulkDeleteView):
     queryset = models.DataFlow.objects.all()
     filterset = filtersets.DataFlowFilterSet
     table = tables.DataFlowTable
-
-
-#
-# As tabs
-#
-
-
-class DataFlowListTabViewBase(generic.ObjectChildrenView):
-    queryset = None
-    child_model = models.DataFlow
-    table = tables.DataFlowTable
-    filterset = filtersets.DataFlowFilterSet
-    template_name = "netbox_data_flows/dataflow_tab.html"
-    actions = ("changelog",)
-
-    tab = ViewTab(
-        label="Data Flows",
-        permission="netbox_data_flows.view_dataflow",
-        badge=lambda obj: models.DataFlow.objects.source_or_destination(
-            obj
-        ).count(),
-        hide_if_empty=True,
-    )
-
-    def get_children(self, request, parent):
-        return models.DataFlow.objects.source_or_destination(
-            parent
-        ).prefetch_related(
-            "application",
-            "application__role",
-            "group",
-        )
-
-
-@register_model_view(Device, name="dataflows-tab", path="dataflows")
-class DeviceDataFlowListTabView(DataFlowListTabViewBase):
-    queryset = Device.objects.all()
-
-
-@register_model_view(VirtualMachine, name="dataflows-tab", path="dataflows")
-class VirtualMachineDataFlowListTabView(DataFlowListTabViewBase):
-    queryset = VirtualMachine.objects.all()
-
-
-@register_model_view(IPAddress, name="dataflows-tab", path="dataflows")
-class IPAddressDataFlowListTabView(DataFlowListTabViewBase):
-    queryset = IPAddress.objects.all()
-
-
-@register_model_view(IPRange, name="dataflows-tab", path="dataflows")
-class IPRangeDataFlowListTabView(DataFlowListTabViewBase):
-    queryset = IPRange.objects.all()
-
-
-@register_model_view(Prefix, name="dataflows-tab", path="dataflows")
-class PrefixDataFlowListTabView(DataFlowListTabViewBase):
-    queryset = Prefix.objects.all()

netbox_data_flows/views/objectaliases.py
@@ -1,11 +1,7 @@
 from django.db.models import Count
 
 from netbox.views import generic
-from utilities.views import ViewTab, register_model_view
-
-from dcim.models import Device
-from ipam.models import IPAddress, IPRange, Prefix
-from virtualization.models import VirtualMachine
+from utilities.views import register_model_view
 
 from netbox_data_flows import filtersets, forms, models, tables
 from netbox_data_flows.utils.aliases import AddAliasesView, RemoveAliasView
@@ -114,54 +110,3 @@ class ObjectAliasRemoveTargetView(RemoveAliasView):
     queryset = models.ObjectAlias.objects.all()
     aliases_attribute = "targets"
     template_name = "netbox_data_flows/objectalias_removetarget.html"
-
-
-#
-# As tabs
-#
-
-
-class ObjectAliasListTabViewBase(generic.ObjectChildrenView):
-    queryset = None
-    child_model = models.ObjectAlias
-    table = tables.ObjectAliasTable
-    filterset = filtersets.ObjectAliasFilterSet
-    template_name = "netbox_data_flows/objectalias_tab.html"
-    actions = ("changelog",)
-
-    tab = ViewTab(
-        label="Object Aliases",
-        permission="netbox_data_flows.view_objectalias",
-        badge=lambda obj: models.ObjectAlias.objects.contains(obj).count(),
-        hide_if_empty=True,
-    )
-
-    def get_children(self, request, parent):
-        return models.ObjectAlias.objects.contains(parent).annotate(
-            target_count=Count("targets"),
-        )
-
-
-@register_model_view(Device, name="objectalias-tab", path="aliases")
-class DeviceObjectAliasListTabView(ObjectAliasListTabViewBase):
-    queryset = Device.objects.all()
-
-
-@register_model_view(VirtualMachine, name="objectalias-tab", path="aliases")
-class VirtualMachineObjectAliasListTabView(ObjectAliasListTabViewBase):
-    queryset = VirtualMachine.objects.all()
-
-
-@register_model_view(IPAddress, name="objectalias-tab", path="aliases")
-class IPAddressObjectAliasListTabView(ObjectAliasListTabViewBase):
-    queryset = IPAddress.objects.all()
-
-
-@register_model_view(IPRange, name="objectalias-tab", path="aliases")
-class IPRangeObjectAliasListTabView(ObjectAliasListTabViewBase):
-    queryset = IPRange.objects.all()
-
-
-@register_model_view(Prefix, name="objectalias-tab", path="aliases")
-class PrefixObjectAliasListTabView(ObjectAliasListTabViewBase):
-    queryset = Prefix.objects.all()

commit 84887a3771b3ccd80954e76ba4e90edf5a5a6b59

Fix duplicate tabs in Device view
Fix bug introduced in a9393caf932e52f3a9d250d42a4af523f898c23b

netbox_data_flows/views/model_tabs.py
@@ -99,8 +99,8 @@ class DataFlowListTabViewBase(generic.ObjectView):
 for model in MODELS:
     # create a subclass of DataFlowListTabViewBase per model
     type(
-        f"{model}DataFlowTabView",
+        f"{model.__name__}DataFlowTabView",
         (DataFlowListTabViewBase,),
         {},
-        model=Device,
+        model=model,
     )