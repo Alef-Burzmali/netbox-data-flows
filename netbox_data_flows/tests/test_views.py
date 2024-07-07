0a1252c22195d2393843d5df5b4aecb6ec7fcb17
Thomas Fargeix <alef-github@burzmali.com>
2024-07-07 22:31:34 +0200

    Add view tests for Data Flows

--- netbox_data_flows/tests/test_views.py
+++ netbox_data_flows/tests/test_views.py
@@ -176,6 +176,163 @@ class DataFlowGroupTestCase(
         super().test_delete_object_with_constrained_permission()
 
 
+class DataFlowTestCase(PluginUrlBase, ViewTestCases.PrimaryObjectViewTestCase):
+    model = models.DataFlow
+
+    @classmethod
+    def setUpTestData(cls):
+        apps = TestData.create_applications()
+        groups = TestData.create_dataflowgroups()
+        aliases = TestData.create_objectaliases()
+        dataflows = TestData.create_dataflows()
+
+        tags = create_tags("Alpha", "Bravo", "Charlie")
+
+        cls.form_data = {
+            "name": "Data Flow X",
+            "description": "A new data flow",
+            "application": apps[0].pk,
+            "group": groups[0].pk,
+            "status": choices.DataFlowStatusChoices.STATUS_ENABLED,
+            "protocol": choices.DataFlowProtocolChoices.PROTOCOL_ICMP,
+            "source_ports": "81,82,83",
+            "destination_ports": "181,182,183",
+            "sources": [a.pk for a in aliases[0:2]],
+            "destinations": [a.pk for a in aliases[3:5]],
+            "comments": "Some comments",
+            "tags": [t.pk for t in tags],
+        }
+
+        enabled = choices.DataFlowStatusChoices.STATUS_ENABLED
+        disabled = choices.DataFlowStatusChoices.STATUS_ENABLED
+
+        proto_any = choices.DataFlowProtocolChoices.PROTOCOL_ANY
+        proto_icmp = choices.DataFlowProtocolChoices.PROTOCOL_ICMP
+        proto_tcp = choices.DataFlowProtocolChoices.PROTOCOL_TCP
+        proto_udp = choices.DataFlowProtocolChoices.PROTOCOL_UDP
+        proto_tcp_udp = choices.DataFlowProtocolChoices.PROTOCOL_TCP_UDP
+        proto_sctp = choices.DataFlowProtocolChoices.PROTOCOL_SCTP
+
+        cls.csv_data = (
+            (
+                "name,description,application,group,status,protocol,"
+                "source_ports,destination_ports,sources,destinations,comments"
+            ),
+            (
+                f"DF 7,Desc 7,{apps[0].name},{groups[0].slug},{enabled},{proto_any},"
+                ",,,,Comments 7"
+            ),
+            (
+                f"DF 8,Desc 8,{apps[1].name},,{disabled},{proto_icmp},"
+                ",,,,Comments 8"
+            ),
+            (
+                f"DF 9,Desc 9,,{groups[2].slug},{enabled},{proto_tcp},"
+                f'"10,20","50-60",{aliases[0].name},{aliases[1].name},'
+            ),
+            (
+                f"DF 11,Desc 10,{apps[1].name},,{disabled},{proto_udp},"
+                f',443,"{aliases[0].name},{aliases[1].name}",,Comments'
+            ),
+            (
+                f"DF 11,Desc 11,,,{enabled},{proto_tcp_udp},"
+                f',443,,"{aliases[2].name},{aliases[3].name}",Comments'
+            ),
+        )
+
+        cls.csv_update_data = (
+            "id,name,description,application,group,status,protocol,"
+            "source_ports,destination_ports,sources,destinations,comments",
+            (
+                f"{dataflows[0].pk},DF 12,Desc 12,{apps[0].name},,{enabled},"
+                f'{proto_sctp},"50-60","10,11",'
+                f"{aliases[0].name},{aliases[0].name},Some comments"
+            ),
+            (
+                f"{dataflows[1].pk},DF 13,Desc 13,,{groups[0].slug},{disabled},"
+                f"{proto_tcp},443,443,"
+                f'"{aliases[0].name},{aliases[0].name}","{aliases[1].name}",'
+                "Some comments"
+            ),
+        )
+
+        cls.bulk_edit_data = {
+            "description": "New description",
+            "comments": "New comments",
+            "application": apps[0].pk,
+            "group": groups[0].pk,
+            "status": choices.DataFlowStatusChoices.STATUS_DISABLED,
+            "protocol": choices.DataFlowProtocolChoices.PROTOCOL_TCP,
+            "source_ports": "66,666,1666",
+            "destination_ports": "33,333,334,335,1333",
+            "sources": [a.pk for a in aliases[5:7]],
+            "destinations": [a.pk for a in aliases[0:3]],
+        }
+
+    @override_settings(EXEMPT_VIEW_PERMISSIONS=["*"], EXEMPT_EXCLUDE_MODELS=[])
+    def test_edit_hyphen_ports(self):
+        instance = models.DataFlow.objects.first()
+
+        # Assign model-level permission
+        obj_perm = ObjectPermission(name="Test permission", actions=["change"])
+        obj_perm.save()
+        obj_perm.users.add(self.user)
+        obj_perm.object_types.add(ObjectType.objects.get_for_model(self.model))
+
+        form_data = dict(**self.form_data)
+        form_data["source_ports"] = "10-15,20-21"
+        form_data["destination_ports"] = "99,100-103"
+
+        changelog_data = dict(**self.form_data)
+        changelog_data["source_ports"] = "10,11,12,13,14,15,20,21"
+        changelog_data["destination_ports"] = "99,100,101,102,103"
+
+        # Try POST with model-level permission
+        request = {
+            "path": self._get_url("edit", instance),
+            "data": post_data(form_data),
+        }
+        self.assertHttpStatus(self.client.post(**request), 302)
+        instance = self._get_queryset().get(pk=instance.pk)
+        self.assertInstanceEqual(instance, changelog_data)
+
+    @override_settings(EXEMPT_VIEW_PERMISSIONS=["*"], EXEMPT_EXCLUDE_MODELS=[])
+    def test_bulk_edit_hyphen_ports(self):
+        pk_list = list(self._get_queryset().values_list("pk", flat=True)[:3])
+        data = {
+            "pk": pk_list,
+            "_apply": True,  # Form button
+        }
+
+        bulk_edit_data = dict(**self.bulk_edit_data)
+        bulk_edit_data["source_ports"] = "10-15,20-21"
+        bulk_edit_data["destination_ports"] = "99,100-103"
+
+        changelog_data = dict(**self.bulk_edit_data)
+        changelog_data["source_ports"] = "10,11,12,13,14,15,20,21"
+        changelog_data["destination_ports"] = "99,100,101,102,103"
+
+        # Append the form data to the request
+        data.update(post_data(bulk_edit_data))
+
+        # Assign model-level permission
+        obj_perm = ObjectPermission(
+            name="Test permission", actions=["view", "change"]
+        )
+        obj_perm.save()
+        obj_perm.users.add(self.user)
+        obj_perm.object_types.add(ObjectType.objects.get_for_model(self.model))
+
+        # Try POST with model-level permission
+        self.assertHttpStatus(
+            self.client.post(self._get_url("bulk_edit"), data), 302
+        )
+        for i, instance in enumerate(
+            self._get_queryset().filter(pk__in=pk_list)
+        ):
+            self.assertInstanceEqual(instance, changelog_data)
+
+
 class ObjectAliasTestCase(
     PluginUrlBase, ViewTestCases.PrimaryObjectViewTestCase
 ):
