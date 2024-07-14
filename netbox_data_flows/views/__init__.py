b9ca1b0958ab8091e474dc4c49084c22cba4ccdd
Thomas Fargeix <alef-github@burzmali.com>
2024-07-14 18:27:40 +0200

    Linting: Add noqa to __init__ that import all submodules

--- netbox_data_flows/views/__init__.py
+++ netbox_data_flows/views/__init__.py
@@ -1,6 +1,7 @@
-from .applicationroles import *  # noqa: F401,F403
-from .applications import *  # noqa: F401,F403
-from .dataflows import *  # noqa: F401,F403
-from .groups import *  # noqa: F401,F403
-from .model_tabs import *  # noqa: F401,F403
-from .objectaliases import *  # noqa: F401,F403
+# flake8: noqa
+from .applicationroles import *
+from .applications import *
+from .dataflows import *
+from .groups import *
+from .model_tabs import *
+from .objectaliases import *
