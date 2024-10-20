bac12b9f94f6c38d0727c3738d59ad10ee3f94f4
Thomas Fargeix <alef-github@burzmali.com>
2024-10-20 16:51:11 +0200

    Release v1.0.5

--- netbox_data_flows/__init__.py
+++ netbox_data_flows/__init__.py
@@ -7,7 +7,7 @@ __version__ = "1.0.5"
 class DataFlowsConfig(PluginConfig):
     name = "netbox_data_flows"
     verbose_name = "Data Flows"
-    description = "NetBox plugin to document data flows between " "systems and applications."
+    description = "NetBox plugin to document data flows between systems and applications."
     version = __version__
     base_url = "data-flows"
     author = "Thomas Fargeix"
