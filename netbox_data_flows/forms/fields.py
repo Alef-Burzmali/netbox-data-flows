f3f9a76fc4fe7b73efdb54bd4a8cdd447c1485f3
Thomas Fargeix <alef-github@burzmali.com>
2024-10-27 16:17:16 +0100

    Fix #43: Format ports as ranges in edit forms

---
+++ netbox_data_flows/forms/fields.py
@@ -0,0 +1,18 @@
+from utilities.data import array_to_string
+from utilities.forms.fields import NumericArrayField as NumericArrayField_
+
+
+__all__ = ("NumericArrayField",)
+
+
+class NumericArrayField(NumericArrayField_):
+    """Format ranges in a human-friendly way before displaying the field."""
+
+    def prepare_value(self, value):
+        if not value:
+            return ""
+
+        if isinstance(value, str):
+            return value
+
+        return array_to_string(value)
